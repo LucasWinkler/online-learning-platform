0da7b75087ed97f9b1ff5774b4ceeb19cc1fa4de
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-06 01:39:40 -0500

    chore: Updated env variables and added prisma seeding - Updated environment variables example file and added corresponding types in env.js - Added prisma seeding file - Added extra database scripts for prisma in package.json

--- src/env.js
+++ src/env.js
@@ -11,12 +11,24 @@ export const env = createEnv({
       .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
-        "You forgot to change the default URL"
+        (str) => !str.includes("YOUR_POOLED_POSTGRESQL_URL_HERE"),
+        "You forgot to change the default URL",
+      ),
+    DATABASE_URL_UNPOOLED: z
+      .string()
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_UNPOOLED_POSTGRESQL_URL_HERE"),
+        "You forgot to change the default URL",
       ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    AUTH0_SECRET: z.string(),
+    AUTH0_BASE_URL: z.string().url(),
+    AUTH0_ISSUER_BASE_URL: z.string().url(),
+    AUTH0_CLIENT_ID: z.string(),
+    AUTH0_CLIENT_SECRET: z.string(),
   },
 
   /**
@@ -34,7 +46,13 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL_UNPOOLED: process.env.DATABASE_URL_UNPOOLED,
     NODE_ENV: process.env.NODE_ENV,
+    AUTH0_SECRET: process.env.AUTH0_SECRET,
+    AUTH0_BASE_URL: process.env.AUTH0_BASE_URL,
+    AUTH0_ISSUER_BASE_URL: process.env.AUTH0_ISSUER_BASE_URL,
+    AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+    AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
