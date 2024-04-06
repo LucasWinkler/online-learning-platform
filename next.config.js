1e7d434981d40b34c4ffbc41c945cb632992ca0f
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-06 01:17:12 -0500

    chore: Installed superjson and next-superjson-plugin@0.6.1

--- next.config.js
+++ next.config.js
@@ -7,15 +7,12 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
+  },
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]],
   },
 };
 
