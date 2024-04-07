40a6c3fc468e4e6587f10244c5e364c7c01302da
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-06 22:09:16 -0500

    feat: Added login handler in [auth0].ts with dynamic redirectUri

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,3 +1,24 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import { HandlerError } from "@auth0/nextjs-auth0";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default handleAuth();
+export default handleAuth({
+  async login(req: NextApiRequest, res: NextApiResponse) {
+    // req.host could be 'foo.domain.com' or 'bar.domain.com'
+    // you can set `BASE_URL` to https://foo.domain.com or https://bar.domain.com
+    const redirectUri = `https://${req.headers.host}/api/auth/callback`;
+    try {
+      await handleLogin(req, res, {
+        authorizationParams: {
+          redirect_uri: redirectUri,
+        },
+      });
+    } catch (error: unknown) {
+      if (error instanceof HandlerError) {
+        res.status(error.status ?? 400).end(error.message);
+      }
+
+      res.status(400).end("Error 400 in /api/auth/[auth0].ts -> handleLogin()");
+    }
+  },
+});
