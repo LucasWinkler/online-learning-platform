359dfac1a2d3b0ea50c984cd474fa22ab7b0f583
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-06 22:44:58 -0500

    feat: Implement custom callback, logout and modified login functions in auth0

--- src/pages/api/auth/[auth0].ts
+++ src/pages/api/auth/[auth0].ts
@@ -1,24 +1,66 @@
-import { handleAuth, handleLogin } from "@auth0/nextjs-auth0";
+import {
+  handleAuth,
+  handleCallback,
+  handleLogin,
+  handleLogout,
+} from "@auth0/nextjs-auth0";
 import { HandlerError } from "@auth0/nextjs-auth0";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { env } from "~/env";
+
+function getUrls(req: NextApiRequest) {
+  const host = req.headers.host;
+  const protocol = process.env.VERCEL_URL ? "https" : "http";
+  const redirectUri = `${protocol}://${host}/api/auth/callback`;
+  const returnTo = `${protocol}://${host}`;
+
+  return {
+    redirectUri,
+    returnTo,
+  };
+}
 
 export default handleAuth({
-  async login(req: NextApiRequest, res: NextApiResponse) {
-    const redirectUri = `${env.NODE_ENV === "development" ? "http" : "https"}://${req.headers.host}/api/auth/callback`;
-
+  async callback(req: NextApiRequest, res: NextApiResponse) {
     try {
+      const { redirectUri } = getUrls(req);
+
+      await handleCallback(req, res, {
+        redirectUri: redirectUri,
+      });
+    } catch (error: unknown) {
+      if (error instanceof HandlerError) {
+        res.status(error.status ?? 500).end(error.message);
+      }
+
+      res.status(500).end("Error in /api/auth/[auth0].ts -> callback()");
+    }
+  },
+
+  async login(req: NextApiRequest, res: NextApiResponse) {
+    try {
+      const { redirectUri, returnTo } = getUrls(req);
+
       await handleLogin(req, res, {
         authorizationParams: {
           redirect_uri: redirectUri,
         },
+
+        returnTo: returnTo,
       });
     } catch (error: unknown) {
       if (error instanceof HandlerError) {
         res.status(error.status ?? 400).end(error.message);
       }
 
-      res.status(400).end("Error 400 in /api/auth/[auth0].ts -> handleLogin()");
+      res.status(400).end("Error in /api/auth/[auth0].ts -> login()");
     }
+  },
+
+  async logout(req: NextApiRequest, res: NextApiResponse) {
+    const { returnTo } = getUrls(req);
+
+    await handleLogout(req, res, {
+      returnTo: returnTo,
+    });
   },
 });
