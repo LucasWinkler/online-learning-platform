e61d7ca8154f1d776d2341068168a9ad3bb33aee
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-27 02:01:50 -0500

    fix: Upgrade next-auth, update environment variables, and correct mail.ts Vercel link protocol

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -5,7 +5,7 @@ import { env } from "~/env";
 const resend = new Resend(env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmationLink = `${env.VERCEL_URL ?? "http://localhost:3000"}/auth/verify-email?token=${token}`;
+  const confirmationLink = `${env.VERCEL_URL ? "https://" : ""}${env.VERCEL_URL ?? "http://localhost:3000"}/auth/verify-email?token=${token}`;
 
   await resend.emails.send({
     from: "Online Learning Platform <noreply@lucaswinkler.dev>",
