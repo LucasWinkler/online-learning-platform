d4678c90e2d0050eb3991ea60d9d91344132ce98
Lucas Winkler <lucaswinkler@gmail.com>
2024-04-29 14:15:18 -0500

    feat: Update authentication middleware

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,28 @@
 /**
+ * The default redirect path after logging in
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/";
+
+/**
  * An array of routes that are accessible to the public
  * These routes do not require authentication
  */
-export const publicRoutes = ["/", "/courses", "/auth/verify-email"];
+export const unprotectedRoutes = ["/", "/courses", "/auth/verify-email"];
 
 /**
  * An array of routes that are used for authentication
- * These routes will redirect logged in users to /my-courses
+ * These routes will redirect logged in users to DEFAULT_LOGIN_REDIRECT
  */
-export const authRoutes = ["/auth/login", "/auth/register", "/auth/error"];
+export const authenticationRoutes = [
+  "/auth/login",
+  "/auth/register",
+  "/auth/error",
+  "/auth/reset",
+  "/auth/new-password",
+];
 
 /**
  * The prefix for all API authentication routes
  * Routes that start with this prefix are used for API authentication purposes
  */
-export const apiAuthPrefix = "/api/auth";
-
-/**
- * The default redirect path after logging in
- */
-export const DEFAULT_LOGIN_REDIRECT = "/";
+export const authApiRoutePrefix = "/api/auth";
