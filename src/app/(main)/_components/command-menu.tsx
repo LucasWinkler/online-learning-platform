57f9c60e349eff29eca3c0c330f4490cd15718ba
Lucas Winkler <lucaswinkler@gmail.com>
2024-05-02 15:27:45 -0500

    feat: Add initial sidebar+header styling - Initial layout and styling of header + sidebar - Contains a command menu in the header for navigation or commands - Sidebar contains temp links and a sign out button - Menu button on mobile is not yet implemented

---
+++ src/app/(main)/_components/command-menu.tsx
@@ -0,0 +1,118 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Role } from "@prisma/client";
+import { CommandIcon, SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+import { Button } from "~/components/ui/button";
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+} from "~/components/ui/command";
+import { useCurrentUser } from "~/hooks/use-current-user";
+import { cn } from "~/lib/utils";
+
+type CommandSearchListGroup = {
+  heading: string;
+  role?: Role;
+  items: { label: string; path: string }[];
+};
+
+export const commandSearchList: CommandSearchListGroup[] = [
+  {
+    heading: "Student Dashboard",
+    items: [
+      { label: "Overview", path: "/" },
+      { label: "All Courses", path: "/courses" },
+      { label: "My Courses", path: "/my-courses" },
+    ],
+  },
+  {
+    heading: "Instructor Dashboard",
+    role: Role.ADMIN,
+    items: [
+      { label: "Overview", path: "/manage/" },
+      { label: "Students", path: "/manage/students" },
+      { label: "Courses", path: "/manage/courses" },
+    ],
+  },
+  {
+    heading: "Settings",
+    items: [
+      { label: "Profile", path: "/settings/profile" },
+      { label: "Account", path: "/settings/account" },
+      { label: "Security", path: "/settings/security" },
+    ],
+  },
+];
+
+type CommandMenuProps = {
+  className?: string;
+};
+
+export const CommandMenu = ({ className }: CommandMenuProps) => {
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const { user } = useCurrentUser();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <Button
+        className={cn("flex w-full justify-between rounded-md", className)}
+        aria-label="Open search menu"
+        variant="outline"
+        size="sm"
+        onClick={() => setOpen(true)}
+      >
+        <span className="flex items-center gap-1">
+          <SearchIcon className="size-4" />
+          <span>Search...</span>
+        </span>
+        <span className="flex gap-1 rounded-sm bg-secondary p-1">
+          <CommandIcon className="size-4" />
+          <span>K</span>
+        </span>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Search..." />
+        <CommandList>
+          <CommandEmpty>No results found...</CommandEmpty>
+          {commandSearchList.map(
+            (group) =>
+              (!group.role || group.role === user?.role) && (
+                <CommandGroup key={group.heading} heading={group.heading}>
+                  {group.items.map((item) => (
+                    <CommandItem
+                      key={item.label}
+                      onClick={() => router.push(item.path)}
+                    >
+                      {item.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              ),
+          )}
+          <CommandSeparator />
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+};
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,32 +1,103 @@
-import { Header } from "~/components/header";
+import { HomeIcon, MenuIcon } from "lucide-react";
+
+import ActiveLink from "~/components/active-link";
+import { LoginButton } from "~/components/auth/login-button";
+import { UserMenu } from "~/components/auth/user-menu";
+import { Link } from "~/components/link";
+import { Logo } from "~/components/logo";
+import { Button } from "~/components/ui/button";
+import { currentUser } from "~/lib/auth";
+
+import { CommandMenu } from "./_components/command-menu";
 
 type MainLayoutProps = {
   children: React.ReactNode;
 };
 
-const MainLayout = ({ children }: MainLayoutProps) => {
+const temp = [
+  {
+    label: "Overview",
+    href: "/",
+  },
+  {
+    label: "All Courses",
+    href: "/courses",
+  },
+  {
+    label: "My Courses",
+    href: "/my-courses",
+  },
+  {
+    label: "Example",
+    href: "/example",
+  },
+  {
+    label: "Example 2",
+    href: "/example-2",
+  },
+  {
+    label: "Example 3",
+    href: "/example-3",
+  },
+];
+
+const MainLayout = async ({ children }: MainLayoutProps) => {
+  const user = await currentUser();
+
   return (
-    <>
-      <Header />
-      <div className="flex h-full flex-row pt-[80px]">
-        <aside className="fixed inset-y-0 z-[9] hidden h-full shrink-0 flex-col border-r border-border bg-background pt-[80px] md:flex md:w-[240px]">
-          <nav className="p-6">
-            <ul>
-              <li>
-                <a>Link</a>
+    <div className="grid h-full min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[240px_1fr] xl:grid-cols-[280px_1fr]">
+      <aside className="hidden h-full max-h-screen flex-col gap-2 border-r border-border bg-background md:flex">
+        <div className="h-header flex items-center justify-start border-b border-border px-4 lg:px-6">
+          <Link className="" href="/">
+            <Logo type="short" />
+          </Link>
+        </div>
+        <nav className="flex-1 flex-col items-start px-2 text-sm font-medium lg:px-4">
+          <ul className="flex flex-col">
+            {temp.map((item, index) => (
+              <li className="" key={index}>
+                <ActiveLink
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 hover:bg-secondary"
+                  activeClassName="bg-secondary"
+                  href={item.href}
+                >
+                  <HomeIcon className="size-4" />
+                  {item.label}
+                </ActiveLink>
               </li>
-              <li>
-                <a>Link</a>
-              </li>
-              <li>
-                <a>Link</a>
-              </li>
-            </ul>
-          </nav>
-        </aside>
-        <main className="flex-1  xs:p-6 md:ml-[240px]">{children}</main>
+            ))}
+          </ul>
+        </nav>
+        <div className="mt-auto p-4 lg:p-6">
+          <Button className="w-full" variant="secondary">
+            Sign out
+          </Button>
+        </div>
+      </aside>
+      <div className="flex flex-col">
+        <header className="h-header flex items-center justify-between gap-2 border-b border-border bg-background px-4 lg:px-6">
+          <Button className="md:hidden" variant="ghost" size="icon">
+            <MenuIcon className="h-6 w-6" />
+          </Button>
+          <CommandMenu className="w-full px-1 md:max-w-[200px]" />
+          <div className="flex items-center justify-end gap-2">
+            {user ? (
+              <UserMenu
+                fullName={user?.name}
+                email={user?.email}
+                avatarImage={user?.image}
+                role={user?.role}
+              />
+            ) : (
+              <>
+                <LoginButton type="redirect">Join us</LoginButton>
+              </>
+            )}
+          </div>
+        </header>
+        <main className="px-4 lg:px-6">{children}</main>
       </div>
-    </>
+    </div>
   );
 };
 
