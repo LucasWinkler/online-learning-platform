603401186aae723acc7801affcaa69dbdcbfa17d
Lucas Winkler <lucaswinkler@gmail.com>
2024-05-04 00:07:42 -0500

    refactor: adjust page loading ui + add basic skeleton ui for settings

--- src/app/(main)/(common)/settings/_components/settings-wrapper.tsx
+++ src/app/(main)/(common)/settings/_components/settings-wrapper.tsx
@@ -4,10 +4,12 @@ type SettingsWrapperProps = {
 };
 
 export const SettingsWrapper = ({ title, children }: SettingsWrapperProps) => {
+  const settingsTitle = `${title} Settings`;
+
   return (
     <div className="flex flex-col gap-4 rounded-xl border border-border bg-background p-6 sm:gap-5 lg:gap-6">
       <h2 className="text-xl font-semibold xxs:text-2xl md:text-3xl">
-        {title} Settings
+        {settingsTitle}
       </h2>
       <div className="flex flex-col gap-6">{children}</div>
     </div>
--- src/app/(main)/(common)/settings/loading.tsx
+++ src/app/(main)/(common)/settings/loading.tsx
@@ -1,10 +1,13 @@
-import { LoaderCircleIcon } from "lucide-react";
+import { Skeleton } from "~/components/ui/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex h-[calc(100vh-var(--header-height)-10rem)] items-center justify-center">
-      <div className="flex h-full items-center justify-center">
-        <LoaderCircleIcon className="size-12 animate-spin text-primary" />
+    <div className="flex flex-col gap-4 rounded-xl border border-border bg-background p-6 sm:gap-5 lg:gap-6">
+      <Skeleton className="h-8 w-40 sm:h-10 sm:w-60" />
+      <div className="flex flex-col gap-6">
+        <Skeleton className="h-52 w-full" />
+        <Skeleton className="h-48 w-full" />
+        <Skeleton className="h-56 w-full" />
       </div>
     </div>
   );
